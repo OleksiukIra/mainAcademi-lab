1e7c65a5d223e2fe353c8743888958793da2c516
Iryna Oleksiuk <Irina.Galuzinskaia@gmail.com>
2019-11-19 10:31:11 +0300

    using onclick, Date object

---
+++ lab_span/button/index.js
@@ -0,0 +1,26 @@
+let span = document.getElementsByClassName('text')[0];
+let newTag = document.createElement('b');
+
+document.getElementById('changed').onclick = function changeText(){
+    span.innerHTML = '<b>' + span.textContent + '</b>';
+    //  span.replaceWith(newTag);
+}
+
+
+
+document.getElementById('btn_result').onclick = function displayResult(){
+    let field_1 = parseInt(document.getElementById('field_1').value);
+let field_2 = parseInt(document.getElementById('field_2').value);
+
+
+let result = field_1 + field_2;
+
+    document.getElementById('result').innerHTML = result;
+}
+
+let arr = ['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня', 'Июля', 'Августа', 'Сентября', 'Октября', 'Ноября', 'Декабря'];
+let date = new Date();
+let day = date.getDate();
+let month =arr[date.getMonth()];
+let year = date.getFullYear(2019);
+document.getElementsByClassName('data_1')[0].innerHTML= day + '.'+ month +'.' + year;
