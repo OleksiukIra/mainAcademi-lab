947ae0bdd85c823541e428d537a6f40cfbd911a7
Iryna Oleksiuk <Irina.Galuzinskaia@gmail.com>
2019-11-27 19:19:12 +0300

    new task width object Date

--- lab_span/button/index.js
+++ lab_span/button/index.js
@@ -10,10 +10,10 @@ document.getElementById('changed').onclick = function changeText(){
 
 document.getElementById('btn_result').onclick = function displayResult(){
     let field_1 = parseInt(document.getElementById('field_1').value);
-let field_2 = parseInt(document.getElementById('field_2').value);
+    let field_2 = parseInt(document.getElementById('field_2').value);
 
 
-let result = field_1 + field_2;
+    let result = field_1 + field_2;
 
     document.getElementById('result').innerHTML = result;
 }
@@ -23,4 +23,17 @@ let date = new Date();
 let day = date.getDate();
 let month =arr[date.getMonth()];
 let year = date.getFullYear(2019);
-document.getElementsByClassName('data_1')[0].innerHTML= day + '.'+ month +'.' + year;
+    document.getElementsByClassName('data_1')[0].innerHTML= day + ' '+ month +' ' + year;
+
+
+    let arr = [1,2,'aasf','1','123',123];
+    function filter_list(l) {
+        let newArr = [];
+        for (let i = 0; i <arr.length; i++){
+        if(typeof arr[i] !=='number'){
+        newArr.splice[i];
+        }
+      }
+      return newArr;
+    }
+console.log(filter_list(l));
